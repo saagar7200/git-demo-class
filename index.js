9eda10c10ca45d54ec59fd30bf5c9a6219d2b737
Sagar Bhandari <rsaagar7200@gmail.com>
2025-07-06 07:57:07 +0545

    index file

---
+++ index.js
@@ -0,0 +1,160 @@
+//? single threaded(single call stack) , dynamic typed (loosely typed ), interpreted (one line at a time) prom. language
+// // syn 
+// // async (event loop)
+
+// // task 1 (4min)
+// //task 2 
+
+//  variables
+//  named storage for values (future use)
+
+// // array =  [s,].
+
+// // Array.
+
+// // 3 methods
+// var ,let, const 
+
+// var x = 3
+// var x = 'hello'
+
+
+
+
+// var x = 20
+// var x = 20
+
+// let y = 10
+
+// let y = 20
+
+// x =10
+// y = 20
+
+// const z = 48
+// const z = 20
+// z =40
+
+// console.log(x,y,z)
+// // let y = 29
+
+
+
+//  x = 40 // reassignment
+
+// // console.log(x)
+
+
+
+//
+// const z = 48
+
+// console.log(z) 
+
+// const y = 10
+//  console.log(y)
+
+//  y = 'hello'
+
+//  console.log(y)
+
+// data types
+
+// primitive
+//! string
+
+let str = 'hello'
+let str1 = "world"
+// template literal (es6)
+const user = ''
+// let greet = `Hello  ${user ? user : "Guest"}`
+// console.log(greet)
+//! number
+
+// const x = 10
+// const y = 10.6
+
+//! boolean
+let bool = false
+
+
+//! undefined
+
+let x;
+
+// console.log(x)
+
+//! null
+
+let y = null;
+// console.log(y)
+
+// non-primitive
+// array
+// objects
+// functions
+
+// coersing
+// implicit 
+// 
+// explicit
+// Boolean()
+// Number
+// String
+// parseInt('')
+
+//! operators (symbol)
+ 
+// ? arithmetic // + ,- ,* /, **, % ..
+// let sum = 10 + 19
+// let num = 10
+// let num2 = 20
+
+// console.log( 2 ** 9)
+// ? assignment
+// = , += ,-=, ...
+// let num = 10 
+
+// // num = num + 1
+
+// num -= 1
+
+// console.log(num)
+
+
+// ? comparison
+//  == , === , != , !== , < > >= >= -> boolean
+
+let a = 10
+let b = '10'
+// console.log(a != b)
+
+// console.log(a !== b)
+
+// console.log('' == 0)
+// truthy values
+// 1 , 'hjbk'
+// falsy values
+// 0 , ''
+
+// console.log(Boolean(undefined))
+// let num = 's'
+// if(num){
+
+// }
+
+
+
+// ? logical 
+// and (&&) , or ( || ) , not (!)
+
+const isStudent = false
+const isUser =false
+console.log(isStudent && isUser)
+console.log(isStudent || isUser)
+
+console.log(!isStudent)
+
+
+
+
