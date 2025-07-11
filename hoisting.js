db5fc3801d0add8d7c18f2682daa7f43c59b5462
Sagar Bhandari <rsaagar7200@gmail.com>
2025-07-11 07:22:46 +0545

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,58 @@
+// var x 
+//  function greet (){
+//     console.log("Hello. Good morning.")
+//  }
+// / let x = 10;
+
+// let x;
+
+// x = 10
+
+// 2 phases
+
+//! 1. compilation (memory creation) phase
+// [crete memory for every variable & function declarations ]
+// Memory
+// x:undefined
+// greet:(){
+//     console.log("Hello. Good morning.")
+//  }
+
+//! 2.execution phase
+// [starts ececuting the code & initialize variables with actual values]
+// Memory
+// x:10
+// greet:(){
+//     console.log("Hello. Good morning.")
+//  }
+
+
+
+
+
+
+console.log(x) // undefined
+
+// var x = 10
+ var x = 10
+
+ console.log(x) //10
+
+
+
+
+greet() //Hello. Good morning.
+
+ function greet (){
+    console.log("Hello. Good morning.")
+ }
+
+
+
+
+
+
+
+
+
+
